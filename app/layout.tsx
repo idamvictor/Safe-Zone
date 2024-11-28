2b34411699b37860180480035f4d84ace5085af9
victor <idamvictorx1@gmail.com>
2024-11-28 21:47:02 +0100

    fix some font issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   weight: "400",
