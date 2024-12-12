a01fd0953c2ac9e8a4939beefbfc13474ac5f039
victor <idamvictorx1@gmail.com>
2024-12-12 19:25:38 +0100

    completed the download section layout

--- components/download-section.tsx
+++ components/download-section.tsx
@@ -54,7 +54,7 @@ const StoreButton: React.FC<StoreButtonProps> = ({
 
 export const DownloadSection: React.FC = () => {
   return (
-    <div className="flex overflow-hidden flex-col justify-center items-center px-20 py-36 w-full border-b-0 bg-secondary border-secondary-foreground max-md:px-5 max-md:py-24 max-md:max-w-full">
+    <div className="flex overflow-hidden flex-col justify-center items-center px-20 py-36 w-full border-b-2 bg-secondary border-secondary-foreground max-md:px-5 max-md:py-24 max-md:max-w-full ">
       <div className="flex flex-col justify-center mb-0 max-w-full w-[612px] max-md:mb-2.5">
         <div className="flex flex-col w-full text-center max-md:max-w-full">
           <div className="text-5xl font-bold leading-none text-secondary-foreground max-md:max-w-full max-md:text-4xl">
@@ -64,7 +64,9 @@ export const DownloadSection: React.FC = () => {
             DOWNLOAD FOR FREE
           </div>
         </div>
-        <div className="flex gap-6 items-center self-center mt-12 max-w-full w-[461px] max-md:mt-10">
+
+
+        <div className="flex flex-wrap justify-center gap-6 items-center self-center mt-12 max-w-full w-[461px] max-md:mt-10">
           {storeData.map((store, index) => (
             <StoreButton
               key={index}
