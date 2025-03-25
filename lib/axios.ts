2adce592b4fb0ca9f9c4d4140806d2e0ec05bcf9
Victor Idam <idamvictorx1@gmail.com>
2025-03-25 11:47:11 +0100

    changes the base url position and added it to the env

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,8 +1,20 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://api.safezone.ng/api/v1",
+//   // baseURL: "/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default axiosInstance;
+
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.safezone.ng/api/v1",
-  // baseURL: "/api",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
